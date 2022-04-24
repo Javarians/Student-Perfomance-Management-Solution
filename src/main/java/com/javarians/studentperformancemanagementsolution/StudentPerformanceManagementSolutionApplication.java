665a36ccc2781be082cdfad14bc0b90943155a7f
Decagon <decagon@Decagons-MacBook-Air.local>
2022-04-24 21:26:41 +0100

    Initialised

---
+++ src/main/java/com/javarians/studentperformancemanagementsolution/StudentPerformanceManagementSolutionApplication.java
@@ -0,0 +1,13 @@
+package com.javarians.studentperformancemanagementsolution;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudentPerformanceManagementSolutionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StudentPerformanceManagementSolutionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/javarians/studentperformancemanagementsolution/StudentPerformanceManagementSolutionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javarians.studentperformancemanagementsolution;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentPerformanceManagementSolutionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
