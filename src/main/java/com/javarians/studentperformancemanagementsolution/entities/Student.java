4bcb6b310e98cb8b52318b7d5965647669dccef1
Deennn <abideenlawal70@gmail.com>
2022-04-24 22:49:00 +0100

    set up some database configuration and added student entity class

---
+++ src/main/java/com/javarians/studentperformancemanagementsolution/entities/Student.java
@@ -0,0 +1,10 @@
+package com.javarians.studentperformancemanagementsolution.entities;
+
+public class Student {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String guardianEmail;
+//    private DateTim
+}
